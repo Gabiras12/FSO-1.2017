d28140b29a9bc94cfeb6c94c1aa37d6be4678c7f
Victor Fernandes <victor.hmfd@gmail.com>
2017-03-24 00:41:41 -0300

    Update ex02
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- job1/ex2/main.c
+++ job1/ex2/main.c
@@ -7,12 +7,12 @@ int main(int argc, char **argv) {
     char *p;
 
     printf("Numero de argumentos: %d\n", argc - 1);
-    printf("Nome do execultavel: %s\n", *(argv));
+    printf("Nome do executavel: %s\n", *(argv));
     for (i = 1; i < argc; i++) {
       printf("Parametro #%d: %s\n", i, *(argv+i));
     }
   } else {
-    printf("Execulte o programa com algum argumento\n");
+    printf("Execute o programa com algum argumento\n");
   }
 
 }
