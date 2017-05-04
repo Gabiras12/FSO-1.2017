161c2890e37d85ac56f3daf1bc57d72868cacc62
Victor Fernandes <victor.hmfd@gmail.com>
2017-05-04 14:39:50 -0300

    Update producer thread
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- job3/main.c
+++ job3/main.c
@@ -18,27 +18,30 @@ void write_to_file(char * message){
   fclose(output_file);
 }
 
-void * generate_random_numbers(void * args){
-    int * vector = (int*) malloc(sizeof(int) * 50);
+void * write_to_buffer(void * args){
+    int * buffer = (int *) args;
     char msg[100];
 
     for(int i=0; i<50; i++){
-      vector[i] = rand();
-      snprintf(msg, 100 , "[producao]: Numero gerado: %d", vector[i]);
+      buffer[i] = rand();
+      snprintf(msg, 100 , "[producao]: Numero gerado: %d", buffer[i]);
       write_to_file(msg);
-      //usleep(100000); //sleep for 100 ms
+      usleep(100000); //sleep for 100 ms
     }
 }
 
-void create_producer_thread(){
+void create_producer_thread(int * buffer){
   pthread_t producer_thread;
-  
-  pthread_create(&producer_thread, NULL, generate_random_numbers, NULL);
+
+  pthread_create(&producer_thread, NULL, write_to_buffer, buffer);
   pthread_join(producer_thread, NULL);
 }
 
+
 int main(int argc, const char * argv[]){
   srand((unsigned)time(NULL));
-  create_producer_thread();
+
+  int * buffer = (int*) malloc(sizeof(int) * 50);
+  create_producer_thread(buffer);
   return 0;
 }
