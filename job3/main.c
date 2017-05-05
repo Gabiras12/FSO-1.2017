13f009c421a1632823545fba653c0ca24418311b
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-05-05 15:36:48 -0300

    Adding file name as param

--- job3/main.c
+++ job3/main.c
@@ -15,16 +15,56 @@ typedef struct {
   int min_number; // smaller number in buffer
 } buffer_t;
 
+char * logFileName;
+
 void write_to_file(char * message);
 int check_max_number(int max_number, int * buffer);
 int check_min_number(int min_number, int * buffer);
 void * write_to_buffer(void * args);
-void * read_to_buffer(void * args);
+void * read_from_buffer(void * args);
 
+int main(int argc, const char **argv){
+
+  // check if log file name was defined by the user
+  if (argv[1] == NULL) {
+    fprintf(stderr, "Log file not defined by user.\nPlease run again passing the file name as a param.\n");
+    return 0;
+  } else {
+    logFileName = argv[1];
+  }
+
+  //Set seed for random numbers
+  srand((unsigned)time(NULL));
+
+  // Create buffer struct and initialize mutex and condition variables
+  buffer_t buffer = {
+    .len = 0,
+    .mutex = PTHREAD_MUTEX_INITIALIZER,
+    .can_produce = PTHREAD_COND_INITIALIZER,
+    .can_consume = PTHREAD_COND_INITIALIZER,
+    .max_number = 0,
+    .min_number = 0
+  };
+
+  // Create threads to write and read
+  pthread_t producer, consumer1, consumer2;
+
+  pthread_create(&producer, NULL, write_to_buffer, (void*)&buffer);
+  pthread_create(&consumer1, NULL, read_from_buffer, (void*)&buffer);
+  pthread_create(&consumer2, NULL, read_from_buffer, (void*)&buffer);
+
+
+  // Wait threads returns to end program
+  pthread_join(producer, NULL);
+  pthread_join(consumer1, NULL);
+  pthread_join(consumer2, NULL);
+
+  return 0;
+}
 
 void write_to_file(char * message){
 
-  FILE * output_file = fopen("teste.txt", "a");
+  FILE * output_file = fopen(logFileName, "a");
 
   if (output_file == NULL) {
     printf("Error opening file!\n");
@@ -83,20 +123,26 @@ void * write_to_buffer(void * args){
 }
 
 
-void * read_to_buffer(void * args){
+// Function to read from buffer when lock available and there is data on it
+void * read_from_buffer(void * args){
+  // cast args back to buffer_t type
   buffer_t *buffer = (buffer_t*) args;
   char msg[100];
 
   while(1){
-    pthread_mutex_lock(&buffer->mutex); /* protect buffer */
+    // Lock the mutex. pthread_mutex_lock works like this:
+    // if the lock is available, the function locks it and returns immediately
+    // if not available wait until lock set available and block the thread
+    pthread_mutex_lock(&buffer->mutex);
 
+    // if len == 0 thre is no data in the buffer
     while(buffer->len == 0) {
       // wait for new items to be appended to the buffer
       pthread_cond_wait(&buffer->can_consume, &buffer->mutex);
     }
 
+    // remove item from buffer
     buffer->len --;
-
     buffer->min_number = check_min_number(buffer->min_number, buffer->buf);
     buffer->max_number = check_max_number(buffer->max_number, buffer->buf);
 
@@ -111,33 +157,4 @@ void * read_to_buffer(void * args){
 
     usleep(150000); //sleep for 150 ms
   }
-}
-
-
-int main(int argc, const char * argv[]){
-
-  srand((unsigned)time(NULL));
-
-  buffer_t buffer = {
-    .len = 0,
-    .mutex = PTHREAD_MUTEX_INITIALIZER,
-    .can_produce = PTHREAD_COND_INITIALIZER,
-    .can_consume = PTHREAD_COND_INITIALIZER,
-    .max_number = 0,
-    .min_number = 0
-  };
-
-  pthread_t producer, consumer1, consumer2;
-  // Initialize the mutex and condition variables
-
-  pthread_create(&producer, NULL, write_to_buffer, (void*)&buffer);
-
-  pthread_create(&consumer1, NULL, read_to_buffer, (void*)&buffer);
-  pthread_create(&consumer2, NULL, read_to_buffer, (void*)&buffer);
-
-  pthread_join(producer, NULL);
-  pthread_join(consumer1, NULL);
-  pthread_join(consumer2, NULL);
-
-  return 0;
 }
