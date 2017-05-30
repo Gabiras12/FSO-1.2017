7f56f4a09fd6179ea15c534de76c9a16b13798c0
Victor Fernandes <victor.hmfd@gmail.com>
2017-05-29 22:51:55 -0300

    q01b finished
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

---
+++ job4/q01b/src/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "libseno.h"
+
+
+int main(int argc, char *argv[]){
+
+  double arg;
+
+  if(argv[1] == NULL) {
+    printf("Parâmetros de entrada não definidos!\n");
+  }
+  else if(strcmp(argv[1],"-a") == 0){
+    arg = atof(argv[2]);
+    printf("arc_seno (%.2f)\n", arc_seno(arg));
+  }
+  else if(strcmp(argv[1],"-s") == 0) {
+    arg = atof(argv[2]);
+    printf("seno (%.2f)\n", seno(arg));
+  }
+  else{
+    printf("Argumento inválido\n");
+  }
+
+}
