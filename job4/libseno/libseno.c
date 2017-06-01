abec9f5dcdd22218c4d8b7ea14a0fb05591e15d6
Victor Fernandes <victor.hmfd@gmail.com>
2017-06-01 20:24:43 -0300

    Update function arcseno
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- job4/libseno/libseno.c
+++ job4/libseno/libseno.c
@@ -5,6 +5,12 @@
 
 #define MAX_TERMS 10
 #define PI 3.1415926535897932384626433832795
+#define A0 1.5707288
+#define A1 -0.2121144
+#define A2 0.0742610
+#define A3 -0.0187293
+
+
 
 unsigned long factorial(unsigned long f)
 {
@@ -24,10 +30,8 @@ double seno(double angulo){
 
 double arc_seno(double seno){
   double arc_seno = 0;
-  double arc_cos = 0;
 
-  arc_cos =  (-0.69813170079773212 * seno * seno - 0.87266462599716477) * seno + 1.5707963267948966;
-  arc_seno = PI/2 - arc_cos;
+  arc_seno = PI/2 - sqrt(1-seno) * (A0 + A1 * seno + A2 * pow(seno,2) + A3 * pow(seno, 3));
 
   return arc_seno;
 }
